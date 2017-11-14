f57d6c064f7e869bc68dbb315e9b0e4f02e8ed31
Giacomo Cerquone <cerquone@gmail.com>
2017-11-14 14:00:00 +0200

    Added different classes for each tab, implementing listview

---
+++ app/src/main/java/gglp/shopifier/Model/Shop.java
@@ -0,0 +1,8 @@
+package gglp.shopifier.Model;
+
+/**
+ * Created by Giacomo on 20/11/2017.
+ */
+
+public class Shop {
+}
--- app/src/main/java/gglp/shopifier/Tab1List.java
+++ app/src/main/java/gglp/shopifier/Tab1List.java
@@ -1,8 +1,15 @@
 package gglp.shopifier;
 
-/**
- * Created by Giacomo on 12/11/2017.
- */
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class Tab1List {
+public class Tab1List extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1list, container, false);
+        return rootView;
+    }
 }
