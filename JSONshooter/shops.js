e2664ec97a70f4dbd591522609e55e6ed25930a4
Giacomo Cerquone <cerquone@gmail.com>
2017-11-23 19:11:24 +0100

    Updated json server

--- JSONshooter/shops.js
+++ JSONshooter/shops.js
@@ -1,33 +1,30 @@
 module.exports = [
   {
     name: 'Mr. robot',
-    location: {
-      lat: 41.1,
-      long: 23.2
-    },
+    lat: 41.1,
+    lon: 23.2,
     image: 'images/shop1.jpg',
     descr: 'Il negozio perfetto per i tuoi acquisti elettronici, vieni a trovarci',
     phone: '338342093',
-    address: 'Via antica arischia'
+    address: 'Via antica arischia',
+    email: 'mr.robot@shop.it'
   }, {
     name: 'Mr. robot',
-    location: {
-      lat: 41.1,
-      long: 23.2
-    },
+    lat: 41.1,
+    lon: 23.2,
     image: 'images/shop1.jpg',
     descr: 'Il negozio perfetto per i tuoi acquisti elettronici, vieni a trovarci',
     phone: '338342093',
-    address: 'Via antica arischia'
+    address: 'Via antica arischia',
+    email: 'mr.robot@shop.it'
   }, {
     name: 'Mr. robot',
-    location: {
-      lat: 41.1,
-      long: 23.2
-    },
+    lat: 41.1,
+    lon: 23.2,
     image: 'images/shop1.jpg',
     descr: 'Il negozio perfetto per i tuoi acquisti elettronici, vieni a trovarci',
     phone: '338342093',
-    address: 'Via antica arischia'
+    address: 'Via antica arischia',
+    email: 'mr.robot@shop.it'
   },
 ]
