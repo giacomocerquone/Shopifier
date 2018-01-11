6c8acb42b820ed9d9a121dae6cd7ed407b99cb63
Luca Pattavina <pattavinaluca@gmail.com>
2018-01-11 17:20:32 +0100

    Added lat e long in shop model
    Collected the lat e long from the json and added to the shop list. MODIFIED THE JSON !!

--- JSONshooter/shops.js
+++ JSONshooter/shops.js
@@ -1,30 +1,57 @@
 module.exports = [
   {
-    name: 'Mr. robot',
-    lat: 41.1,
-    lon: 23.2,
+    name: 'Alessandro de Amicis ',
+    lat: 42.361929,
+    lon: 13.405602,
     image: 'images/shop1.jpg',
-    descr: 'Il negozio perfetto per i tuoi acquisti elettronici, vieni a trovarci',
-    phone: '338342093',
-    address: 'Via antica arischia',
-    email: 'mr.robot@shop.it'
+    descr: 'So nu cujon',
+    phone: '3917214982',
+    address: 'Via Filippo Palizzi, 7',
+    email: 'ale.dmc@gmail.it'
   }, {
-    name: 'Mr. robot',
-    lat: 41.1,
-    lon: 23.2,
+    name: 'Alessandro Miconi',
+    lat: 42.356988,
+    lon: 13.412915,
     image: 'images/shop1.jpg',
-    descr: 'Il negozio perfetto per i tuoi acquisti elettronici, vieni a trovarci',
-    phone: '338342093',
-    address: 'Via antica arischia',
-    email: 'mr.robot@shop.it'
+    descr: 'So più cujon de quell de prim',
+    phone: '3461202941',
+    address: 'Via Guglielmo Enrico Fritzche',
+    email: 'ale.mic@gmail.com'
   }, {
-    name: 'Mr. robot',
-    lat: 41.1,
-    lon: 23.2,
+    name: 'Alessio Vacca',
+    lat: 42.348208,
+    lon: 13.404207,
     image: 'images/shop1.jpg',
-    descr: 'Il negozio perfetto per i tuoi acquisti elettronici, vieni a trovarci',
-    phone: '338342093',
-    address: 'Via antica arischia',
-    email: 'mr.robot@shop.it'
+    descr: 'Futuro vigile del fuoco pelatissimo',
+    phone: '3484366708',
+    address: 'Via Carlo Chiarizia 33',
+    email: 'ale.vacca@gmail.com'
+  }, {
+    name: 'Cerquone Giacomo',
+    lat: 42.374507,
+    lon: 13.356093,
+    image: 'images/shop1.jpg',
+    descr: 'esperto programmatore e futuro nobel per la pace',
+    phone: '3408825392',
+    address: 'Via antica arischia 42',
+    email: 'giaco.cer@gmail.com'
+  }, {
+    name: 'Andre Ranaldo',
+    lat: 42.364682,
+    lon: 13.342044,
+    image: 'images/shop1.jpg',
+    descr: 'esperto giocatore di The west, futuro barista al Chromie',
+    phone: '3312566473',
+    address: 'Via Madonna delle Grazie 16',
+    email: 'andrea.rana@gmail.com'
+  }, {
+    name: 'Davide Teodori',
+    lat: 42.370306,
+    lon: 13.340665,
+    image: 'images/shop1.jpg',
+    descr: 'Crimiale, terrorista, futuro detenuto 41bis',
+    phone: '3772319119',
+    address: 'Via Civita Retenga',
+    email: 'davide.teod@gmail.com'
   },
 ]
--- app/src/main/java/gglp/shopifier/Model/Shop.java
+++ app/src/main/java/gglp/shopifier/Model/Shop.java
@@ -6,12 +6,17 @@ public class Shop {
     private String address;
     private String tel;
     private String email;
+    private String lat;
+    private String lon;
 
-    public Shop(String name, String address, String tel, String email) {
+
+    public Shop(String name, String address, String tel, String email,String lat,String lon) {
         setName(name);
         setAddress(address);
         setTel(tel);
         setEmail(email);
+        setLat(lat);
+        setLon(lon);
     }
 
     public String getName() {
@@ -23,17 +28,21 @@ public class Shop {
     public String getTel() {
         return tel;
     }
-    public String getEmail() {
-        return email;
-    }
+    public String getEmail() {return email; }
+    public String getLat() {return lat; }
+    public String getLon() {return lon; }
+
 
     public void setName(String name) { this.name = name; }
     public void setAddress(String address) { this.address = address; }
     public void setTel(String tel) { this.tel = tel; }
     public void setEmail(String email) { this.email = email; }
+    public void setLat(String lat) { this.lat = lat; }
+    public void setLon(String lon) { this.lon = lon; }
+
 
     @Override
     public String toString() {
-        return this.name + ' ' + this.address;
+        return this.name + ' ' + this.address ;
     }
 }
