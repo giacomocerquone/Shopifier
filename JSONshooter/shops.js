29314e3b626ff156578f92b23b8b24a75c6c821f
Luca Pattavina <pattavinaluca@gmail.com>
2018-01-27 19:05:44 +0100

    Added right shops and images

--- JSONshooter/shops.js
+++ JSONshooter/shops.js
@@ -1,55 +1,55 @@
 module.exports = [
   {
-    name: 'Alessandro de Amicis ',
+    name: 'Decathlon ',
     lat: 42.361929,
     lon: 13.405602,
-    image: 'images/shop1.jpg',
-    descr: 'So nu cujon',
+    image: 'images/Decathlon.jpg',
+    descr: 'Il negozio degli sportivi !',
     phone: '3917214982',
     address: 'Via Filippo Palizzi, 7',
-    email: 'ale.dmc@gmail.it'
+    email: 'info@decathlon.it'
   }, {
-    name: 'Alessandro Miconi',
+    name: 'FootLocker',
     lat: 42.356988,
     lon: 13.412915,
-    image: 'images/shop1.jpg',
-    descr: 'So più cujon de quell de prim',
+    image: 'images/FootLocker.jpg',
+    descr: 'Il negozio di scarpe più fornito dell'Aquila!',
     phone: '3461202941',
     address: 'Via Guglielmo Enrico Fritzche',
-    email: 'ale.mic@gmail.com'
+    email: 'info@footlocker.com'
   }, {
-    name: 'Alessio Vacca',
+    name: 'Gucci',
     lat: 42.348208,
     lon: 13.404207,
-    image: 'images/shop1.jpg',
-    descr: 'Futuro vigile del fuoco pelatissimo',
+    image: 'images/Gucci.jpg',
+    descr: 'Il negozio d'alta moda più conoscito del mondo!',
     phone: '3484366708',
     address: 'Via Carlo Chiarizia 33',
-    email: 'ale.vacca@gmail.com'
+    email: 'info@gucci.com'
   }, {
-    name: 'Cerquone Giacomo',
+    name: 'H&M',
     lat: 42.374507,
     lon: 13.356093,
-    image: 'images/shop1.jpg',
-    descr: 'esperto programmatore e futuro nobel per la pace',
+    image: 'images/H&M.jpg',
+    descr: 'H&M ti aspetta con incredibili saldi ed occasioni da non perdere!',
     phone: '3408825392',
     address: 'Via antica arischia 42',
     email: 'giaco.cer@gmail.com'
   }, {
-    name: 'Andre Ranaldo',
+    name: 'MediaWorld',
     lat: 42.364682,
     lon: 13.342044,
-    image: 'images/shop1.jpg',
-    descr: 'esperto giocatore di The west, futuro barista al Chromie',
+    image: 'images/MediaWorld.jpg',
+    descr: 'Il negozio di elettronica preferito dagli italiani!',
     phone: '3312566473',
     address: 'Via Madonna delle Grazie 16',
     email: 'andrea.rana@gmail.com'
   }, {
-    name: 'Davide Teodori',
+    name: 'Target',
     lat: 42.370306,
     lon: 13.340665,
-    image: 'images/shop1.jpg',
-    descr: 'Crimiale, terrorista, futuro detenuto 41bis',
+    image: 'images/Target.jpg',
+    descr: 'La serigrafia più veloce dell'Aquila!',
     phone: '3772319119',
     address: 'Via Civita Retenga',
     email: 'davide.teod@gmail.com'
